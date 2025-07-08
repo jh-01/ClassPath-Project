177dbb4750cdf660d8f1286c8cf19e34aed2772e
imtaehun <luxlaetitia9542@gmail.comHHpackage>
2025-07-08 11:38:31 +0900

    feat: 회원가입 엔드포인트 생성 및 dto ì 생성

--- src/main/java/com/example/classpath/domain/user/controller/UserController.java
+++ src/main/java/com/example/classpath/domain/user/controller/UserController.java
@@ -1,9 +1,32 @@
 package com.example.classpath.domain.user.controller;
 
+import com.example.classpath.domain.user.dto.UserRegistrationRequestDto;
+import com.example.classpath.domain.user.service.UserService;
+import com.example.classpath.global.common.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<Void>> registerUser(
+            @RequestBody @Valid UserRegistrationRequestDto request
+            ) {
+        userService.registerUser(request);
+        return ResponseEntity.
+                status(HttpStatus.CREATED)
+                .body(ApiResponse.success("회원가입이 완료되었습니다.", null));
+    }
+
 }
