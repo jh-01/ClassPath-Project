83cd9e18804047d9317b254c301fb11fc6f82f0c
노트북 <were1117@gmail.com>
2025-07-08 11:03:20 +0900

    feat: @EnableJpaAuditing 추가 -> JPA auditing 활성화

--- src/main/java/com/example/classpath/ClassPathApplication.java
+++ src/main/java/com/example/classpath/ClassPathApplication.java
@@ -2,8 +2,10 @@ package com.example.classpath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ClassPathApplication {
 
     public static void main(String[] args) {
