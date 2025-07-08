652ca71f2f3d700ea0464541772f0e7939a69ba6
노트북 <were1117@gmail.com>
2025-07-08 09:24:39 +0900

    Initial commit

---
+++ src/main/java/com/example/classpath/ClassPathApplication.java
@@ -0,0 +1,13 @@
+package com.example.classpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassPathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassPathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classpath/ClassPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassPathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
