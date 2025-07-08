0d4a3f139c407cf32c176bc8c7f677956f8b2f91
dain <0901ekdls@gmail.com>
2025-07-09 03:36:40 +0900

    feat: 수강신청 관련 커스텀 예외 ErrorType 등록

--- src/main/java/com/example/classpath/global/exception/ErrorType.java
+++ src/main/java/com/example/classpath/global/exception/ErrorType.java
@@ -9,6 +9,14 @@ public enum ErrorType {
     DUPLICATE_USERNUMBER(HttpStatus.CONFLICT, "이미 가입된 유저입니다."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호입니다."),
 
+    // 강의
+    LECTURE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 강의를 찾을 수 없습니다."),
+
+    // 수강신청
+    ALREADY_ENROLLED(HttpStatus.CONFLICT, "이미 수강 신청한 강의입니다."),
+    ENROLLMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "수강 신청 내역을 찾을 수 없습니다."),
+    ENROLLMENT_PERIOD_CLOSED(HttpStatus.BAD_REQUEST, "수강신청 기간이 아닙니다."),
+
 
     ;
 
