bf2f4a46d32a19e13a356e3ac82244cfe263f8f8
dain <0901ekdls@gmail.com>
2025-07-09 03:10:12 +0900

    feat: 수강신청 기간 등록 API 구현(@PostMapping)

---
+++ src/main/java/com/example/classpath/domain/enrollmentperiod/controller/EnrollmentPeriodController.java
@@ -0,0 +1,29 @@
+package com.example.classpath.domain.enrollmentperiod.controller;
+
+import com.example.classpath.domain.enrollmentperiod.dto.request.EnrollmentPeriodRequest;
+import com.example.classpath.domain.enrollmentperiod.entity.EnrollmentPeriod;
+import com.example.classpath.domain.enrollmentperiod.service.EnrollmentPeriodService;
+import com.example.classpath.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/enrollment-period")
+public class EnrollmentPeriodController {
+    private final EnrollmentPeriodService enrollmentPeriodService;
+
+    // 수강신청 기간 설정
+    @PostMapping("/{id}")
+    public ApiResponse<Void> createPeriod(@PathVariable Long id, @RequestBody EnrollmentPeriodRequest request) {
+        enrollmentPeriodService.createPeriod(id, request);
+        return ApiResponse.success("수강신청 기간이 설정되었습니다.", null);
+    }
+}
