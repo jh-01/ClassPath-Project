31cbfaccd674353795e145aefcc61746c077c07a
dain <0901ekdls@gmail.com>
2025-07-08 18:41:01 +0900

    feat: 수강신청 기간 엔티티 생성
    - 수강 신청 기간 정보를 담는 EnrollmentPeriod 엔티티 추가
    - startAt, endAt 필드 포함

---
+++ src/main/java/com/example/classpath/domain/enrollmentperiod/entity/EnrollmentPeriod.java
@@ -0,0 +1,32 @@
+package com.example.classpath.domain.enrollmentperiod.entity;
+
+import com.example.classpath.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class EnrollmentPeriod extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(nullable = false)
+    private LocalDateTime endAt;
+}
