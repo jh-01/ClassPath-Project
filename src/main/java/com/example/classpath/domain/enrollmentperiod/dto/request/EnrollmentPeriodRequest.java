e4dd94131f884f154c2645bfce34bace2d655c00
dain <0901ekdls@gmail.com>
2025-07-08 19:15:55 +0900

    feat: 수강신청 기간 요청 DTO 생성
    - startAt, endAt 필드 포함한 EnrollmentPeriodRequest 생성
    - 유효성 검증을 위한 @NotNull 어노테이션 추가

---
+++ src/main/java/com/example/classpath/domain/enrollmentperiod/dto/request/EnrollmentPeriodRequest.java
@@ -0,0 +1,19 @@
+package com.example.classpath.domain.enrollmentperiod.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class EnrollmentPeriodRequest {
+    @NotNull(message = "시작일은 필수입니다.")
+    private final LocalDateTime startAt;
+
+    @NotNull(message = "종료일은 필수입니다.")
+    private final LocalDateTime endAt;
+
+    public EnrollmentPeriodRequest(LocalDateTime startAt, LocalDateTime endAt) {
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+}
