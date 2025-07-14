dd1c8ae5b00c22d6dedfa599a8f2011d27452279
seoyeon <dlatjdus0303@naver.com>
2025-07-14 15:46:02 +0900

    feat: 인증 방식 SecurityContextHolder로 변경 및 admin url 추가

--- src/main/java/com/example/classpath/domain/lecture/controller/LectureController.java
+++ src/main/java/com/example/classpath/domain/lecture/controller/LectureController.java
@@ -11,29 +11,31 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/lectures")
+@RequestMapping
 @RequiredArgsConstructor
 public class LectureController {
     private final LectureService lectureService;
 
-    @PostMapping
+    @PostMapping("/admin/lectures")
     public ResponseEntity<ApiResponse<LectureResponse>> createLecture(@Valid @RequestBody LectureCreateRequest requestDto) {
         LectureResponse response = lectureService.createLecture(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("강의를 개설하였습니다.", response));
     }
 
-    @DeleteMapping("{lectureId}")
+    @DeleteMapping("/admin/lectures/{lectureId}")
     public ResponseEntity<ApiResponse<Void>> deleteLecture(@PathVariable Long lectureId) {
         lectureService.deleteLecture(lectureId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponse.success("강의를 삭제했습니다.", null));
     }
 
-    @PatchMapping("{lectureId}")
+    @PatchMapping("/admin/lectures/{lectureId}")
     public ResponseEntity<ApiResponse<LectureResponse>> updateLecture(@PathVariable Long lectureId,
                                                                       @Valid @RequestBody LectureUpdateRequest requestDto) {
         LectureResponse response = lectureService.updateLecture(lectureId, requestDto);
@@ -44,7 +46,7 @@ public class LectureController {
     /**
      * 강의명, 강의번호로 검색
      */
-    @GetMapping("/search")
+    @GetMapping("/lectures/search")
     public ResponseEntity<ApiResponse<Page<LectureResponse>>> searchLecture(@ModelAttribute LectureSearchCondition condition,
                                                                             @PageableDefault(size = 20) Pageable pageable) {
         Page<LectureResponse> page = lectureService.searchLecture(condition, pageable);
@@ -54,7 +56,7 @@ public class LectureController {
     /**
      * 강의 단건 조회
      */
-    @GetMapping("{lectureId}")
+    @GetMapping("/lectures/{lectureId}")
     public ResponseEntity<ApiResponse<LectureResponse>> getLecture(@PathVariable Long lectureId) {
         LectureResponse response = lectureService.getLecture(lectureId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("강의 정보를 조회했습니다.", response));
@@ -63,7 +65,7 @@ public class LectureController {
     /**
      * 강의 전체 조회
      */
-    @GetMapping
+    @GetMapping("/lectures")
     public ResponseEntity<ApiResponse<List<LectureResponse>>> getLectures() {
         List<LectureResponse> response = lectureService.getLectures();
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("강의 정보를 조회했습니다.", response));
@@ -72,8 +74,10 @@ public class LectureController {
     /**
      * 신청한 강의 조회
      */
-    @GetMapping("/my")
-    public ResponseEntity<ApiResponse<List<LectureResponse>>> getMyLectures(@RequestParam Long userId) { //TODO 추후에 @ContextHolder에서 꺼낼 예정
+    @GetMapping("/lectures/me")
+    public ResponseEntity<ApiResponse<List<LectureResponse>>> getMyLectures() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long userId = (Long) authentication.getPrincipal();
         List<LectureResponse> response = lectureService.getMyLectures(userId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("나의 강의 정보를 조회했습니다.", response));
     }
@@ -81,7 +85,7 @@ public class LectureController {
     /**
      * 강의별 수강생 목록 조회
      */
-    @GetMapping("/{lectureId}/students")
+    @GetMapping("/admin/lectures/{lectureId}/students")
     public ResponseEntity<ApiResponse<Page<StudentResponse>>> getStudentsByLecture(@PathVariable Long lectureId,
                                                                                    @PageableDefault(size = 20) Pageable pageable) {
         Page<StudentResponse> page = lectureService.getStudentsByLecture(lectureId, pageable);
