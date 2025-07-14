457c4def119a4d6e97dfa87ad772f158f3038aa8
seoyeon <dlatjdus0303@naver.com>
2025-07-15 01:24:08 +0900

    feat: 스케줄링 기능 활성화를 위해 @EnableScheduling 추가

--- src/main/java/com/example/classpath/ClassPathApplication.java
+++ src/main/java/com/example/classpath/ClassPathApplication.java
@@ -3,9 +3,11 @@ package com.example.classpath;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class ClassPathApplication {
 
     public static void main(String[] args) {
