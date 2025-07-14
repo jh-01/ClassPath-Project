9eddd64378e83bcc89b5fb27a0073982789b5cdb
노트북 <were1117@gmail.com>
2025-07-14 19:12:07 +0900

    document: deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 APP_DIR=/home/ec2-user/app
+
+echo "Building app..."
+cd $APP_DIR
+./gradlew build || { echo "Build failed"; exit 1; }
+
 JAR_NAME=$(ls $APP_DIR/build/libs/*.jar | tail -n 1)
 
 echo "Stopping existing app..."
